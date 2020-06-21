2ae1ab683a4dec57f38d1478ff67663df8a3fb10
Victor Idongesit <louvalearn@gmail.com>
2020-06-21 11:32:12 +0100

    input expand

--- index.js
+++ index.js
@@ -1,81 +1,88 @@
 console.log("I just started");
-let context = 'dialogflow';
+let context = "dialogflow";
 const timer = document.getElementById("time");
 
 timer.innerText = new Date().toLocaleTimeString();
 
 const createBotMessage = (messages) => {
-    let body = "";
-    messages.forEach((ms, i) => {
-        body += `
-        <div class="chat bot ${i == 0 ? 'first' : ''} ${i == messages.length - 1 ? '' : "more"}"> ${ms} <span>${new Date().toLocaleTimeString()}</span></div>`
-    });
-    return body;
+  let body = "";
+  messages.forEach((ms, i) => {
+    body += `
+        <div class="chat bot ${i == 0 ? "first" : ""} ${
+      i == messages.length - 1 ? "" : "more"
+    }"> ${ms} <span>${new Date().toLocaleTimeString()}</span></div>`;
+  });
+  return body;
 };
 
 const sendBot = (messages) => {
-    const loader = document.getElementById('loader');
-    const chatBody = document.getElementById('chat-body');
-    const messageBody = createBotMessage(messages);
-    if(loader) {
-        chatBody.removeChild(loader);
-    }
-    chatBody.innerHTML += messageBody;
-    chatBody.scrollTop = chatBody.scrollHeight;
-}
+  const loader = document.getElementById("loader");
+  const chatBody = document.getElementById("chat-body");
+  const messageBody = createBotMessage(messages);
+  if (loader) {
+    chatBody.removeChild(loader);
+  }
+  chatBody.innerHTML += messageBody;
+  chatBody.scrollTop = chatBody.scrollHeight;
+};
 
 sendBot([
-    "Welcome! I am Idad",
-    "How may I help you?"
-    //  "Please reply with your name if you so want to be addressed or say skip"
-]
-
-);
+  "Welcome! I am Idad",
+  "How may I help you?",
+  //  "Please reply with your name if you so want to be addressed or say skip"
+]);
 
 const createUserMessage = (message) => {
-    return `<div class="chat user"> ${message} <span>${new Date().toLocaleTimeString()}</span></div>`;
+  return `<div class="chat user"> ${message} <span>${new Date().toLocaleTimeString()}</span></div>`;
 };
 
 const sendUser = (message) => {
-    const chatBody = document.getElementById('chat-body');
-    const messageBody = createUserMessage(message);
-
-    chatBody.innerHTML += messageBody;
-    chatBody.innerHTML += '<img src="./Images/loading.gif" alt="loader" id="loader">'
-    chatBody.scrollTop = chatBody.scrollHeight;
-
-}
-
-const callBotApi = (value) => {
-    fetch("https://dialogflow-service-idad.herokuapp.com", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-             },
-      
-        body: JSON.stringify({
-            text: value
-        }),
-    }).then(res => res.json()).then(data => {
-        sendBot([data.response]);
-      });
+  const chatBody = document.getElementById("chat-body");
+  const messageBody = createUserMessage(message);
+  const buttons = document.getElementById("buttons");
+  buttons.classList.remove("typing");
+  chatBody.innerHTML += messageBody;
+  chatBody.innerHTML +=
+    '<img src="./Images/loading.gif" alt="loader" id="loader">';
+  chatBody.scrollTop = chatBody.scrollHeight;
 };
 
+const callBotApi = (value) => {
+  fetch("https://dialogflow-service-idad.herokuapp.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
 
+    body: JSON.stringify({
+      text: value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      sendBot([data.response]);
+    });
+};
 
 const manageUserInput = (e) => {
-    const keyCode = e.keyCode || e.which;
-    const value = e.target.value.trim();
-    if (keyCode == 13 && value.length) {
+  const keyCode = e.keyCode || e.which;
+  const keyvalue = e.target.value;
+  const buttons = document.getElementById("buttons");
+  if (keyvalue.length > 1) {
+    buttons.classList.add("typing");
+  } else {
+    buttons.classList.remove("typing");
+  }
 
-        sendUser(value);
-        e.target.value = "";
-        switch (context) {
-            case "within":
-                return;
-            case "dialogflow":
-                return callBotApi(value);
-        }
+  const value = keyvalue.trim();
+  if (keyCode == 13 && value.length) {
+    sendUser(value);
+    e.target.value = "";
+    switch (context) {
+      case "within":
+        return;
+      case "dialogflow":
+        return callBotApi(value);
     }
-}
-
+  }
+};
